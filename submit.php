f0c688edf847ded1f8278339aebe9990eac80c51
Jack Coyle <coylejack1@berkeley.edu>
2018-02-09 17:01:56 -0800

    semicolon life

--- submit.php
+++ submit.php
@@ -2,8 +2,8 @@
 $name = $_POST['name'];
 $sid = $_POST['sid'];
 $secret_word = $_POST['word'];
-$left = $_POST['sid-left']
-$right = $_POST['sid-right']
+$left = $_POST['sid-left'];
+$right = $_POST['sid-right'];
 $real_word = file_get_contents("./secretword.txt");
 if ($secret_word != $real_word) {
     echo "<h3>You got the secret word wrong!</h3><p>Try again, or go to class!</p>";
@@ -21,5 +21,5 @@ $today_attendance = $attendance[$today] ?: array();
 $today_attendance[$sid] = $record;
 $attendance[$today] = $today_attendance;
 file_put_contents("./attendance.json", json_encode($attendance));
-echo "<h3>Thanks!</h3><p>Your attendance has been recorded.</p>"
+echo "<h3>Thanks!</h3><p>Your attendance has been recorded.</p>";
 ?>

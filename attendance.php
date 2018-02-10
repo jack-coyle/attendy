798c99a06834db9979b72a3412c70bf8fd670558
Jack Coyle <coylejack1@berkeley.edu>
2018-02-09 17:19:54 -0800

    View attendance

---
+++ attendance.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Attendance Listing</title>
+</head>
+<body>
+<table>
+<tr>
+<th>Date</th>
+<th>Name</th>
+<th>SID</th>
+</tr>
+<?php
+$attendance = json_decode(file_get_contents("./attendance.php"), true);
+$days = array();
+foreach ($attendance as $key => $value) {
+    foreach ($value as $record) {
+    echo "<tr><td>"+$key$+'</td><td>'+$record['name']+'</td><td>'+$record['sid']+'</td></tr>';
+    }
+}
+</table>
+</body>
+</html>

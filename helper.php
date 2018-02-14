b3b6881bf41e0c6b51faff138c3939427e6315fd
Jack Coyle <coylejack1@berkeley.edu>
2018-02-14 13:53:36 -0800

    Client shit

--- helper.php
+++ helper.php
@@ -1,8 +1,19 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
 /**
  * Returns an authorized API client.
  * @return Google_Client the authorized client object
  */
+ define('APPLICATION_NAME', '171 Attendance Jack Coyle');
+ define('CREDENTIALS_PATH', __DIR__ . '/171.attendance.json');
+ define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
+ // If modifying these scopes, delete your previously saved credentials
+ // at ~/.credentials/sheets.googleapis.com-php-quickstart.json
+ define('SCOPES', implode(' ', array(
+   "https://www.googleapis.com/auth/spreadsheets")
+ ));
+
 function getClient() {
   $client = new Google_Client();
   $client->setApplicationName(APPLICATION_NAME);
@@ -11,33 +22,19 @@ function getClient() {
   $client->setAccessType('offline');
 
   // Load previously authorized credentials from a file.
-  $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
-  if (file_exists($credentialsPath)) {
-    $accessToken = json_decode(file_get_contents($credentialsPath), true);
+  if (file_exists(CREDENTIALS_PATH)) {
+    $accessToken = json_decode(file_get_contents(CREDENTIALS_PATH), true);
   } else {
-    // Request authorization from the user.
-    $authUrl = $client->createAuthUrl();
-    printf("Open the following link in your browser:\n%s\n", $authUrl);
-    print 'Enter verification code: ';
-    $authCode = trim(fgets(STDIN));
-
-    // Exchange authorization code for an access token.
-    $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-
-    // Store the credentials to disk.
-    if(!file_exists(dirname($credentialsPath))) {
-      mkdir(dirname($credentialsPath), 0700, true);
-    }
-    file_put_contents($credentialsPath, json_encode($accessToken));
-    printf("Credentials saved to %s\n", $credentialsPath);
+    echo "error";
   }
   $client->setAccessToken($accessToken);
 
   // Refresh the token if it's expired.
   if ($client->isAccessTokenExpired()) {
     $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-    file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+    file_put_contents((CREDENTIALS_PATH), json_encode($client->getAccessToken()));
   }
   return $client;
 }
+
 ?>
